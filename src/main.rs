cb424bfcb7eda5a8c6dfaaa04017ae958914b225
Alexander Dickhans <adickhans@gmail.com>
2025-03-08 10:34:08 -0700

    Add signing keys to gitignore

---
+++ src/main.rs
@@ -0,0 +1,118 @@
+use std::collections::HashMap;
+use std::sync::Arc;
+use robotevents::client;
+use robotevents::query::{DivisionMatchesQuery, EventsQuery, PaginatedQuery, TeamSkillsQuery, TeamsQuery};
+use robotevents::schema::Division;
+use tokio::sync::RwLock;
+use warp::{http, Filter};
+use serde::{Deserialize, Serialize};
+use tokio::join;
+use tokio::time::sleep_until;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct DeviceSubscription {
+    competition_id: u32,
+    division_id: u32,
+    device_token: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Hash, Eq)]
+struct CompetitionDivisionPair {
+    competition_id: u32,
+    division_id: u32,
+}
+
+impl CompetitionDivisionPair {
+    fn new(competition_id: u32, division_id: u32) -> Self {
+        Self {
+            competition_id,
+            division_id,
+        }
+    }
+
+    fn from_device(device: &DeviceSubscription) -> Self {
+        Self {
+            competition_id: device.competition_id,
+            division_id: device.division_id,
+        }
+    }
+
+}
+
+#[derive(Debug, Clone)]
+struct StateStore {
+    subscriptions: Arc<RwLock<HashMap<CompetitionDivisionPair, Vec<String>>>>,
+    matches: Arc<RwLock<HashMap<CompetitionDivisionPair, Vec<robotevents::schema::Match>>>>,
+}
+
+impl StateStore {
+    fn new() -> Self {
+        Self {
+            subscriptions: Arc::new(RwLock::new(HashMap::new())),
+            matches: Arc::new(RwLock::new(HashMap::new())),
+        }
+    }
+
+    async fn add_subscription_from_device(&self, device: DeviceSubscription) {
+        println!("Adding subscription for competition {:?} and device {}", CompetitionDivisionPair::from_device(&device), device.device_token);
+        let mut subscriptions = self.subscriptions.write().await;
+        let entry = subscriptions.entry(CompetitionDivisionPair::from_device(&device)).or_insert(Vec::new());
+        entry.push(device.device_token);
+    }
+}
+
+async fn add_device(
+    device: DeviceSubscription,
+    state_store: StateStore,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    // let r = competition.grocery_list.read();
+    // Ok(warp::reply::json(&*r))
+    state_store.add_subscription_from_device(device).await;
+    Ok(warp::reply::with_status(
+        "Added device",
+        http::StatusCode::CREATED,
+    ))
+}
+
+fn json_body() -> impl Filter<Extract = (DeviceSubscription,), Error = warp::Rejection> + Clone {
+    // When accepting a body, we want a JSON body
+    // (and to reject huge payloads)...
+    warp::body::content_length_limit(1024 * 16).and(warp::body::json())
+}
+
+async fn poll(state_store: StateStore) {
+    loop {
+        sleep_until(tokio::time::Instant::now() + tokio::time::Duration::from_secs(10)).await;
+
+        // just print information about each subscription
+        let subscriptions = state_store.subscriptions.read().await;
+        for (competition_id, devices) in subscriptions.iter() {
+            println!("Competition {:?}: {:?}", competition_id, devices);
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token = std::env::var("ROBOTEVENTS_TOKEN").expect("ROBOTEVENTS_TOKEN not set");
+
+    let client = client::RobotEvents::new(token);
+
+    let store = StateStore::new();
+    let cloned_store = store.clone();
+    let store_filter = warp::any().map(move || cloned_store.clone());
+
+    let add_items = warp::post()
+        .and(warp::path("v1"))
+        .and(warp::path("subscribe"))
+        .and(warp::path::end())
+        .and(json_body())
+        .and(store_filter.clone())
+        .and_then(add_device);
+
+    join!(
+        warp::serve(add_items)
+            .run(([127, 0, 0, 1], 3030)),
+        poll(store.clone())
+    );
+}
