b7f887e9f35dd8e0f9e28b36679634c2dca84633
Alexander Dickhans <adickhans@gmail.com>
2025-03-08 12:49:31 -0700

    Added support for changing the name of items in the request

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use std::sync::Arc;
+use a2::DefaultNotificationBuilder;
 use robotevents::client;
 use robotevents::query::{DivisionMatchesQuery, EventsQuery, PaginatedQuery, TeamSkillsQuery, TeamsQuery};
 use robotevents::schema::Division;
@@ -14,6 +15,12 @@ struct DeviceSubscription {
     competition_id: u32,
     division_id: u32,
     device_token: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct DeviceSubscriptionChangeRequest {
+    new_device_token: String,
+    old_device_token: String,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Hash, Eq)]
@@ -59,6 +66,25 @@ impl StateStore {
         let entry = subscriptions.entry(CompetitionDivisionPair::from_device(&device)).or_insert(Vec::new());
         entry.push(device.device_token);
     }
+
+    async fn change_subscription_from_device(&self, device: &DeviceSubscriptionChangeRequest) {
+        // find the old subscription and remove it, storing where it was
+        let mut subscriptions = self.subscriptions.write().await;
+        let mut old_competition_division = None;
+
+        for (competition_division, devices) in subscriptions.iter_mut() {
+            if devices.contains(&device.old_device_token) {
+                devices.retain(|x| x != &device.old_device_token);
+                old_competition_division = Some(competition_division.clone());
+            }
+        }
+
+        // add the new subscription
+        if let Some(competition_division) = old_competition_division {
+            let entry = subscriptions.entry(competition_division).or_insert(Vec::new());
+            entry.push(device.new_device_token.clone());
+        }
+    }
 }
 
 async fn add_device(
@@ -74,7 +100,24 @@ async fn add_device(
     ))
 }
 
-fn json_body() -> impl Filter<Extract = (DeviceSubscription,), Error = warp::Rejection> + Clone {
+async fn change_device(
+    device: DeviceSubscriptionChangeRequest,
+    state_store: StateStore,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    state_store.change_subscription_from_device(&device).await;
+    Ok(warp::reply::with_status(
+        "Changed device",
+        http::StatusCode::ACCEPTED,
+    ))
+}
+
+fn json_body_new_device() -> impl Filter<Extract = (DeviceSubscription,), Error = warp::Rejection> + Clone {
+    // When accepting a body, we want a JSON body
+    // (and to reject huge payloads)...
+    warp::body::content_length_limit(1024 * 16).and(warp::body::json())
+}
+
+fn json_body_change_device() -> impl Filter<Extract = (DeviceSubscriptionChangeRequest,), Error = warp::Rejection> + Clone {
     // When accepting a body, we want a JSON body
     // (and to reject huge payloads)...
     warp::body::content_length_limit(1024 * 16).and(warp::body::json())
@@ -106,13 +149,23 @@ async fn main() {
         .and(warp::path("v1"))
         .and(warp::path("subscribe"))
         .and(warp::path::end())
-        .and(json_body())
+        .and(json_body_new_device())
         .and(store_filter.clone())
         .and_then(add_device);
 
+    let change_device = warp::post()
+        .and(warp::path("v1"))
+        .and(warp::path("change"))
+        .and(warp::path::end())
+        .and(json_body_change_device())
+        .and(store_filter.clone())
+        .and_then(change_device);
+
+    let mut builder = DefaultNotificationBuilder::new();
+
     join!(
-        warp::serve(add_items)
-            .run(([127, 0, 0, 1], 3030)),
+        warp::serve(add_items.or(change_device))
+            .run(([0, 0, 0, 0], 3030)),
         poll(store.clone())
     );
 }
